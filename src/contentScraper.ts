cbcd3558f50ca4c8800cf9c2d7df640c4ee08152
Joshua Chen <joshuaz.chen@mail.utoronto.ca>
2026-02-05 09:15:57 -0500

    Update contentScraper.ts

--- src/contentScraper.ts
+++ src/contentScraper.ts
@@ -2,8 +2,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "scrape") {
     
     const pageText = document.body.innerText;
-    sendResponse({status: "success", data: pageText}); // return if complete
-  } else {
+    sendResponse({status: "success", data: pageText});
+  } else { // not sure about how necessary this is since if it cant scrape itll just not work in a really obvious way.
+    // TODO: change this handling s.t. it'll try again
     sendResponse({status: "failed scraping (1)", data: null})
   }
 });
